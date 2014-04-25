92998e66d652520a733c34890c01d2ca1c2b1807
4970chun <futa.yoshida@gmail.com>
2014-04-25 10:36:57 +0900

    移動スクリプトの追加

--- Assets/lightmove.cs
+++ Assets/lightmove.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class lightmove : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+	    public float speed;
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+    	Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+  	}
 }
